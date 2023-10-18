c46ccfe2fd588de737b0392369da2a249e22c8a1
harolditoc <harold.chambilla@usil.pe>
2023-10-18 11:16:47 -0500

    Omisión de login para vistas

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Usuario;
 use App\Form\RegistrationFormType;
+use App\Repository\UsuarioRepository;
 use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
@@ -101,22 +102,31 @@ class RegistrationController extends AbstractController
         ]);
     }
     #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
+    public function verifyUserEmail(Request $request, TranslatorInterface $translator, UsuarioRepository $usuarioRepository): Response
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        // $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $id = $request->get('id');
 
-        // validate email confirmation link, sets User::isVerified=true and persists
+        if (null === $id) {
+            return $this->redirectToRoute('app_proveedor');
+        }
+
+        $user = $usuarioRepository->find($id);
+
+        if (null === $user) {
+            return $this->redirectToRoute('app_proveedor');
+        }
         try {
-            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+            $this->emailVerifier->handleEmailConfirmation($request, $user);
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_proveedor');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_proveedor');
     }
 }
